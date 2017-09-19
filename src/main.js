cd6f10253b9f0c08868779c11af8760539326cf3
Fredrik Fossum <fredrik.fossum@finn.no>
2017-09-19 16:21:40 +0200

    Add validation

--- src/main.js
+++ src/main.js
@@ -2,27 +2,51 @@
 import type { FormsState } from './reducer';
 import reducer from './reducer';
 import { changeValue, CHANGE_VALUE } from './actions';
+import { createSelector } from 'reselect';
+import { mapValues, values, every } from 'lodash';
 
 type State = {
   forms: FormsState,
 };
 
 type Params = {
+  formName: string,
   name: string,
   validators?: {
-    [key: string]: Function,
+    [key: string]: Array<*> | Function,
   },
 };
 export function input(params: Params) {
-  return function(formName: string) {
-    return {
-      name: params.name,
-      getValue(state: State) {
-        const form = state.forms[formName];
-        const input = form && form.inputs && form.inputs[params.name];
-        return input && input.value;
-      },
-    };
+  const { formName, name, validators = {} } = params;
+
+  function getValue(state: State) {
+    const form = state.forms[formName];
+    const input = form && form.inputs && form.inputs[params.name];
+    return input && input.value;
+  }
+
+  const validationSelectors = mapValues(validators, v => {
+    if (typeof v === 'function') {
+      return createSelector(getValue, v);
+    } else {
+      const args = [getValue, ...v];
+      return createSelector.apply(undefined, args);
+    }
+  });
+
+  const getErrors = (state: State) =>
+    mapValues(validationSelectors, f => !f(state));
+
+  const isValid = createSelector(...values(validationSelectors), (...args) =>
+    every(args)
+  );
+
+  return {
+    formName,
+    name,
+    getErrors,
+    getValue,
+    isValid,
   };
 }
 
--- src/main.test.js
+++ src/main.test.js
@@ -2,39 +2,78 @@
 /* eslint-env jest */
 
 import { input } from './main';
+import formsReducer from './reducer';
+import { changeValue } from './actions';
+import { combineReducers } from 'redux';
 
 describe('input', () => {
+  let reducer;
+  let formName = 'formName';
+  let name = 'inputName';
   let state;
-  let formName;
-  let name;
-  let value;
 
   beforeEach(() => {
-    formName = 'form';
-    name = 'input';
-    value = 'abc';
-    state = {
-      forms: {
-        [formName]: {
-          inputs: {
-            [name]: {
-              value,
-            },
-          },
-        },
-      },
-    };
+    reducer = combineReducers({ forms: formsReducer });
+    const initAction: any = { type: '@@INIT' };
+    state = reducer(undefined, initAction);
   });
 
   it('provides its name', () => {
     const result = input({
+      formName,
       name,
-    })(formName);
+    });
     expect(result.name).toBe(name);
   });
 
   it('provides selector for input value', () => {
-    const result = input({ name })(formName);
-    expect(result.getValue(state)).toBe(value);
+    const result = input({ name, formName });
+    state = reducer(state, changeValue({ formName, name, value: 'abc' }));
+    expect(result.getValue(state)).toBe('abc');
+  });
+
+  test('simple validator', () => {
+    const result = input({
+      name,
+      formName,
+      validators: {
+        simple: value => value === 'abc',
+      },
+    });
+    expect(result.isValid(state)).toBe(false);
+    state = reducer(state, changeValue({ formName, name, value: 'abc' }));
+    expect(result.isValid(state)).toBe(true);
+  });
+
+  test('advanced validator', () => {
+    const input1 = input({
+      name: 'input1',
+      formName,
+    });
+    const input2 = input({
+      name: 'input2',
+      formName,
+      validators: {
+        largest: [input1.getValue, (value, input1Value) => value > input1Value],
+      },
+    });
+
+    state = reducer(
+      state,
+      changeValue({ formName: input1.formName, name: input1.name, value: 1 })
+    );
+    state = reducer(
+      state,
+      changeValue({ formName: input2.formName, name: input2.name, value: 2 })
+    );
+
+    expect(input2.isValid(state)).toBe(true);
+
+    state = reducer(
+      state,
+      changeValue({ formName: input1.formName, name: input1.name, value: 3 })
+    );
+
+    expect(input2.isValid(state)).toBe(false);
   });
 });
